47b50e0abaa4ce6e8be31f5969e371f89a89cfcf
kvadrage <kvadrage@gmail.com>
2018-03-01 14:42:59 +0300

    500 bgp sessions template split between 2 servers

--- subint_gen.sh
+++ subint_gen.sh
@@ -6,67 +6,46 @@ then
   exit -1
 fi
 
-echo "Generating 512 vlan interfaces for $1"
+if [ $# -gt 1 ]
+then
+  let ref=$2
+else
+  let ref=100
+fi
+
+echo "Generating 256 vlan interfaces for $1 (3rd octet = $ref+)"
 for i in `seq 2 4 256`;
 do
     let j=i+0-1
+    let sub=ref+1
     ip link add link $1 name $1.$j type vlan id $j
     ip link set dev $1.$j up
-    ip addr add 10.252.101.$i/30 dev $1.$j
+    ip addr add 10.252.$sub.$i/30 dev $1.$j
 done
 
 for i in `seq 2 4 256`;
 do
     let j=i+256-1
+    let sub=ref+2
     ip link add link $1 name $1.$j type vlan id $j
     ip link set dev $1.$j up
-    ip addr add 10.252.102.$i/30 dev $1.$j
+    ip addr add 10.252.$sub.$i/30 dev $1.$j
 done
 
 for i in `seq 2 4 256`;
 do
     let j=i+512-1
+    let sub=ref+3
     ip link add link $1 name $1.$j type vlan id $j
     ip link set dev $1.$j up
-    ip addr add 10.252.103.$i/30 dev $1.$j
+    ip addr add 10.252.$sub.$i/30 dev $1.$j
 done
 
 for i in `seq 2 4 256`;
 do
     let j=i+768-1
+    let sub=ref+4
     ip link add link $1 name $1.$j type vlan id $j
     ip link set dev $1.$j up
-    ip addr add 10.252.104.$i/30 dev $1.$j
-done
-
-for i in `seq 2 4 256`;
-do
-    let j=i+1024-1
-    ip link add link $1 name $1.$j type vlan id $j
-    ip link set dev $1.$j up
-    ip addr add 10.252.105.$i/30 dev $1.$j
-done
-
-for i in `seq 2 4 256`;
-do
-    let j=i+1280-1
-    ip link add link $1 name $1.$j type vlan id $j
-    ip link set dev $1.$j up
-    ip addr add 10.252.106.$i/30 dev $1.$j
-done
-
-for i in `seq 2 4 256`;
-do
-    let j=i+1536-1
-    ip link add link $1 name $1.$j type vlan id $j
-    ip link set dev $1.$j up
-    ip addr add 10.252.107.$i/30 dev $1.$j
-done
-
-for i in `seq 2 4 256`;
-do
-    let j=i+1792-1
-    ip link add link $1 name $1.$j type vlan id $j
-    ip link set dev $1.$j up
-    ip addr add 10.252.108.$i/30 dev $1.$j
+    ip addr add 10.252.$sub.$i/30 dev $1.$j
 done
